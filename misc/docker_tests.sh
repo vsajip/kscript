78b74b12632f4cdc1c8eb81a0ac2a41b3d853638
Holger Brandl <holgerbrandl@gmail.com>
2017-10-19 14:49:08 +0200

    impl docker container for testing

--- misc/docker_tests.sh
+++ misc/docker_tests.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+
+
 # todo push image to docker registry
-docker run -it bioinfo_base
+cd ${KSCRIPT_HOME}/misc
+#docker build --no-cache -t kscript_tester .
+docker build -t kscript_tester .
+docker run -it kscript_tester
 
 #curl -Lso /bin/kscript https://raw.githubusercontent.com/holgerbrandl/kscript/abb5f4c6ee72ec90d22c0fe913284e92363cad0e/kscript && chmod u+x /bin/kscript
-curl -Lso /bin/kscript https://www.dropbox.com/s/l5g8vr0wz78y3zy/kscript?dl=1 && chmod u+x /bin/kscript
+#curl -Lso /bin/kscript https://www.dropbox.com/s/l5g8vr0wz78y3zy/kscript?dl=1 && chmod u+x /bin/kscript
 
-kscript --help
-kscript --self-update
+#kscript --help
+#kscript --self-update
 
 
 ## or using github repo
@@ -17,6 +22,13 @@ export KSCRIPT_HOME=$(pwd)
 
 gradle shadowJar && cp build/libs/kscript-0.1-SNAPSHOT-all.jar kscript.jar
 
+export PATH=$(pwd):${PATH}
 kscript --clear-cache
 
+wget https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh
+chmod u+x assert.sh
+
 ${KSCRIPT_HOME}/test/test_suite.sh
+
+## copy newer jar into container
+#scp brandl@scicomp-mac-12-usb:/Users/brandl/projects/kotlin/kscript/kscript.jar .
