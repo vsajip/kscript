dc20b5653867a01002ba8eca7c80dcee0394e9a9
Holger Brandl <holgerbrandl@gmail.com>
2017-11-08 01:29:36 +0100

    refined docker test setup

--- misc/docker_tests.sh
+++ misc/docker_tests.sh
@@ -9,6 +9,12 @@ docker build -t kscript_tester .
 docker run -it kscript_tester
 
 
+## or with host-local repo-copy
+cd ~/Desktop/
+git clone https://github.com/holgerbrandl/kscript.git kscript_docker
+docker run -it --rm -v $(pwd)/kscript_docker:/kscript kscript_tester
+
+
 #docker rm  `docker ps -q -l` # restart it in the background
 docker start  `docker ps -q -l` # restart it in the background
 docker attach `docker ps -q -l` # reattach the terminal & stdin
@@ -27,13 +33,32 @@ export KSCRIPT_HOME=$(pwd)
 
 gradle shadowJar && cp build/libs/kscript-0.1-SNAPSHOT-all.jar kscript.jar
 
-export PATH=$(pwd):${PATH}
-kscript --clear-cache
-
 wget https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh
 chmod u+x assert.sh
 
+export PATH=$(pwd):${PATH}
+kscript --clear-cache
+
+
 ${KSCRIPT_HOME}/test/test_suite.sh
+
+
+## manuallt test dependency lookup
+./kscript --clear-cache
+resolve_deps() { kotlin -classpath kscript.jar kscript.app.DepedencyUtilKt "$@";}
+
+resolve_deps log4j:log4j:1.2.14
+
 
 ## copy newer jar into container
 #scp brandl@scicomp-mac-12-usb:/Users/brandl/projects/kotlin/kscript/kscript.jar .
+
+
+##https://github.com/moby/moby/issues/26872
+##https://stackoverflow.com/questions/38532483/where-is-var-lib-docker-on-mac-os-x
+
+
+## network address docker ps && docker inspect <id> -> IPAdress
+#https://stackoverflow.com/questions/17157721/how-to-get-a-docker-containers-ip-address-from-the-host
+#sshfs -o root@172.17.0.3:/ /Users/brandl/Desktop/docker_container/ #-o idmap=user -o uid=1001 -o gid=1001
+#sshfs -o 172.17.0.3:/ /Users/brandl/Desktop/docker_container/ #-o idmap=user -o uid=1001 -o gid=1001
