e9e5e3c92675f64b08c7ecf2aedc7a99188124f5
Holger Brandl <holgerbrandl@gmail.com>
2017-11-15 21:03:38 +0100

    inc version to v2.2.1

--- src/main/kotlin/kscript/app/Kscript.kt
+++ src/main/kotlin/kscript/app/Kscript.kt
@@ -19,7 +19,7 @@ import kotlin.system.exitProcess
  * @author Holger Brandl
  */
 
-const val KSCRIPT_VERSION = "2.2.0"
+const val KSCRIPT_VERSION = "2.2.1"
 
 val USAGE = """
 kscript - Enhanced scripting support for Kotlin on *nix-based systems.
