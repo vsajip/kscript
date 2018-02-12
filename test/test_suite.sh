2d68023527df50c60481042a3aad447eadbf3272
Holger Brandl <brandl@mpi-cbg.de>
2018-02-12 09:49:20 +0100

    fixed packaging unit test

--- test/test_suite.sh
+++ test/test_suite.sh
@@ -204,10 +204,10 @@ assert "${KSCRIPT_HOME}/test/resources/script_in_pckg.kts" "I live in a package!
 
 
 ## can we resolve relative imports when using tmp-scripts  (see #95)
-assert "kscript --package test/resources/package_example.kts &>/dev/null; ./package_example 1" "package_me_args_1_mem_4772593664"
+assert "rm -f ./package_example; kscript --package test/resources/package_example.kts &>/dev/null; ./package_example 1" "package_me_args_1_mem_4772593664"
 
 ## https://unix.stackexchange.com/questions/17064/how-to-print-only-last-column
-assert 'eval $(kscript --package "println(args.size)" 2>&1 | tail -n1 | grep -o '\S\+$') three arg uments' "3"
+assert 'rm -f kscriptlet*; cmd=$(kscript --package "println(args.size)" 2>&1 | tail -n1 | cut -f 5 -d " "); $cmd three arg uments' "3"
 
 
 
