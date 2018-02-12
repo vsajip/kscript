2489a2472342f8f159274b302149aef2284ae5c2
Holger Brandl <brandl@mpi-cbg.de>
2018-02-12 14:42:03 +0100

    inc version to 2.4.0

--- src/main/kotlin/kscript/app/Kscript.kt
+++ src/main/kotlin/kscript/app/Kscript.kt
@@ -20,7 +20,7 @@ import kotlin.system.exitProcess
  * @author Holger Brandl
  */
 
-const val KSCRIPT_VERSION = "2.3.0"
+const val KSCRIPT_VERSION = "2.4.0"
 
 val selfName = System.getenv("CUSTOM_KSCRIPT_NAME") ?: "kscript"
 
