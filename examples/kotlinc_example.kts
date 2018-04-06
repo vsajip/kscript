d3bcb130fe6dc5db7238b88e210da5c604005324
Holger Brandl <brandl@mpi-cbg.de>
2018-04-06 09:37:30 +0200

    Clarified intent of `kotlinc_example.kts` (fixes #117)

--- examples/kotlinc_example.kts
+++ examples/kotlinc_example.kts
@@ -1,5 +1,11 @@
 #!/usr/bin/env kotlinc -script
 
+// This is NOT a kscript example but demonstrates how kotlin scripting would work without kscript.
+// In the shebang line we point to kotlinc in scripting mode. Note, that this will just work in some flavors of bash.
+
+// If `args` are showing up as red in Intellij, you've stumbled over an intellij bug.
+// See https://youtrack.jetbrains.com/issue/KT-15019
+
 println("Hello from Kotlin!")
 for (arg in args) {
     println("arg: $arg")
