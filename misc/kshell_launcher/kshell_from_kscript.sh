e0ad3387e6467ee3de7358ac856f2164e924f171
Holger Brandl <holgerbrandl@users.noreply.github.com>
2018-09-28 06:28:21 +0200

    updated depedndencies in kshell launcher

--- misc/kshell_launcher/kshell_from_kscript.sh
+++ misc/kshell_launcher/kshell_from_kscript.sh
@@ -8,7 +8,19 @@ fi
 
 tmpfile=$(mktemp).kts
 
-echo '@file:Include("https://git.io/fAJ5h")' >> $tmpfile
+#echo '@file:Include("https://git.io/fAJ5h")' >> $tmpfile
+echo '
+@file:DependsOn("org.apache.hadoop:hadoop-common:2.7.0")
+
+// should be now on maven central
+@file:DependsOn("com.github.khud:kshell-repl-api:0.2.4-1.2.60")
+
+@file:DependsOn("sparklin:jline3-shaded:0.2-SNAPSHOT")
+
+//@file:DependsOn("sparklin:kshell:0.2-SNAPSHOT")
+@file:DependsOn("sparklin:kshell:0.2.5")
+
+' > $tmpfile
 echo '' >> $tmpfile
 
 argScript=$1
@@ -28,4 +40,4 @@ kscript_nocall() { kotlin -classpath ${KSCRIPT_DIR}/kscript.jar kscript.app.Kscr
 kshellCP=$(kscript_nocall $tmpfile | cut -d' ' -f4)
 
 ## create new
-exec java -classpath "${kshellCP}" sparklin.kshell.KotlinShell $@
+java -classpath "${kshellCP}" com.github.khud.sparklin.kshell.KotlinShell $@
