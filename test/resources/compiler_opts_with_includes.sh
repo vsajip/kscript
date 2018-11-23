45c4e1ab15d65e854a10dbc965fe06b02a0f31fb
Holger Brandl <holgerbrandl@users.noreply.github.com>
2018-11-23 16:59:12 +0100

    replaced append with create redirect when creating test-script

--- test/resources/compiler_opts_with_includes.sh
+++ test/resources/compiler_opts_with_includes.sh
@@ -2,7 +2,7 @@
 
 echo "fun foo() = 42" > compiler_opts_with_includes_dep.kts
 
-cat <<"EOF" >> compiler_opts_with_includes_master.kt
+cat <<"EOF" > compiler_opts_with_includes_master.kt
 @file:CompilerOpts("-jvm-target 1.8")
 @file:Include("compiler_opts_with_includes_dep.kts")
 @file:DependsOnMaven("log4j:log4j:1.2.14")
