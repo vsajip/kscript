3eb42fa574941a383a725833629348cc7387b0fd
Holger Brandl <holgerbrandl@users.noreply.github.com>
2018-12-14 14:00:41 +0100

    Use resource from repo for resolve boostrap header

--- src/main/kotlin/kscript/app/Kscript.kt
+++ src/main/kotlin/kscript/app/Kscript.kt
@@ -67,7 +67,7 @@ private val BOOTSTRAP_HEADER = """
 
     //usr/bin/env echo '
     /**** BOOTSTRAP kscript ****\'>/dev/null
-    command -v kscript >/dev/null 2>&1 || curl -L "https://git.io/fpVLG" | bash 1>&2
+    command -v kscript >/dev/null 2>&1 || curl -L "https://git.io/fpF1K" | bash 1>&2
     exec kscript $0 "$@"
     \*** IMPORTANT: Any code including imports and annotations must come after this line ***/
 
