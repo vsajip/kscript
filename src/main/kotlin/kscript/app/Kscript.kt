d6673eb5d26df5080f306004b51938fbf9cc01fa
holgerbrandl@gmail.com <holgerbrandl@users.noreply.github.com>
2020-01-06 08:48:47 +0100

    Updated copyright year to 2020

--- src/main/kotlin/kscript/app/Kscript.kt
+++ src/main/kotlin/kscript/app/Kscript.kt
@@ -45,7 +45,7 @@ Options:
  --add-bootstrap-header  Prepend bash header that installs kscript if necessary
 
 
-Copyright : 2019 Holger Brandl
+Copyright : 2020 Holger Brandl
 License   : MIT
 Version   : v$KSCRIPT_VERSION
 Website   : https://github.com/holgerbrandl/kscript
