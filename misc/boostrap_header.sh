5859f5487f32dc18e74c9dcdc0adbdaaf21dbe98
Patrick Yin <eetech@gmail.com>
2020-08-23 12:39:59 -0700

    Fix bootstrap script env variable issue (#271)

--- misc/boostrap_header.sh
+++ misc/boostrap_header.sh
@@ -2,7 +2,7 @@ function _in_path() { command -v "$1" >/dev/null 2>&1; }
 _in_path kscript && echo "kscript is already installed at $(which kscript)" 1>&2 || {
     function echo_and_eval() { echo "$ $@" 1>&2; eval "$@" 1>&2; }
     _in_path sdk || {
-        curl "https://get.sdkman.io" | bash 1>&2 && \
+        export SDKMAN_DIR="$HOME/.sdkman" && curl "https://get.sdkman.io" | bash 1>&2 && \
             echo_and_eval source "$SDKMAN_DIR/bin/sdkman-init.sh"
     }
 
