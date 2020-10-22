35c1862dbb47489efd5e109702ec6d0da762b010
Andreas Rossbacher <andreas.rossbacher@udo.edu>
2020-10-22 05:13:46 -0700

    Update gradle to 6.7 (#282)
    Update com.github.johnrengelman.shadow to latest
    Fix any warnings that were created by the new gradle version
    
    Co-authored-by: Andreas Rossbacher <andreas.rossbacher@airbnb.com>
    Co-authored-by: Holger Brandl <holgerbrandl@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ group = "com.github.holgerbrandl.kscript.launcher"
 dependencies {
     compile("com.offbytwo:docopt:0.6.0.20150202")
 
-    compile("com.jcabi:jcabi-aether:0.10.1") {
+    implementation("com.jcabi:jcabi-aether:0.10.1") {
         exclude("org.hibernate", "hibernate-validator")
         exclude("org.slf4j", "slf4j-api")
         exclude("org.slf4j", "jcl-over-slf4j")
@@ -19,12 +19,12 @@ dependencies {
         exclude("org.kuali.maven.wagons", "maven-s3-wagon")
     }
     // compile("com.jcabi:jcabi-aether:0.10.1:sources") //can be used for debugging, but somehow adds logging to dependency resolvement?
-    compile("org.apache.maven:maven-core:3.0.3")
-    compile("org.slf4j:slf4j-nop:1.7.25")
+    implementation("org.apache.maven:maven-core:3.0.3")
+    implementation("org.slf4j:slf4j-nop:1.7.25")
 
-    testCompile("junit:junit:4.12")
-    testCompile( "io.kotlintest:kotlintest:2.0.7")
-    testCompile(kotlin("script-runtime"))
+    testImplementation("junit:junit:4.12")
+    testImplementation( "io.kotlintest:kotlintest:2.0.7")
+    testImplementation(kotlin("script-runtime"))
 }
 
 repositories {
@@ -33,11 +33,11 @@ repositories {
 
 val shadowJar by tasks.getting(ShadowJar::class) {
     // set empty string to classifier and version to get predictable jar file name: build/libs/kscript.jar
-    archiveName = "kscript.jar"
+    archiveFileName.set("kscript.jar")
     doLast {
         copy {
             from(File(projectDir, "src/kscript"))
-            into(archivePath.parentFile)
+            into(archiveFile.get().asFile.parentFile)
         }
     }
 }
