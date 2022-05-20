fd211ece2d201bc46be5c0441919c28ac8492b0d
Marcin <aarti@interia.pl>
2022-05-20 20:11:20 +0200

    Added comment.

--- src/main/kotlin/kscript/app/model/OsType.kt
+++ src/main/kotlin/kscript/app/model/OsType.kt
@@ -9,6 +9,9 @@ enum class OsType(val osName: String) {
 
     companion object {
         fun findOrThrow(name: String) =
+            //Exact comparison (it.osName.equals(name, true)) seems to be not feasible as there is also e.g. "darwin21"
+            //and maybe even other osTypes, but specific versions of os'es shouldn't belong to OsType.
+            //https://github.com/holgerbrandl/kscript/issues/356
             values().find { name.contains(it.osName, true) } ?: throw IllegalArgumentException("Unsupported OS: $name")
     }
 }
