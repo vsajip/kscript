b25fbb1543f87e999f64e9535142e5c03e63e4e3
Marcin <aarti@interia.pl>
2022-05-24 16:56:07 +0200

    build: updated bugfix version refactor: more strict OS type checking

--- src/main/kotlin/kscript/app/model/OsType.kt
+++ src/main/kotlin/kscript/app/model/OsType.kt
@@ -8,9 +8,12 @@ enum class OsType(val osName: String) {
     fun isUnixHostedOnWindows() = (this == CYGWIN || this == MSYS)
 
     companion object {
-        fun findOrThrow(name: String) =
-            //Exact comparison (it.osName.equals(name, true)) seems to be not feasible as there is also e.g. "darwin21"
-            //and maybe even other osTypes: specific versions of os'es shouldn't belong to OsType.
-            values().find { name.contains(it.osName, true) } ?: throw IllegalArgumentException("Unsupported OS: $name")
+        fun findOrThrow(name: String): OsType {
+            // Exact comparison (it.osName.equals(name, true)) seems to be not feasible as there is also e.g. "darwin21"
+            // "darwin19" and maybe even other osTypes. It seems though that startsWith() is covering all cases.
+            // https://github.com/holgerbrandl/kscript/issues/356
+            return values().find { name.startsWith(it.osName, true) }
+                ?: throw IllegalArgumentException("Unsupported OS: $name")
+        }
     }
 }
