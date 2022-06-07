2c8e57a513762de9e571438f93997711f3c1b148
Marcin <aarti@interia.pl>
2022-06-07 16:09:02 +0200

    bugfix: fix for OSTYPE linux-musl which is reported on Alpine Linux.

--- src/main/kotlin/kscript/app/model/OsType.kt
+++ src/main/kotlin/kscript/app/model/OsType.kt
@@ -1,9 +1,9 @@
 package kscript.app.model
 
 enum class OsType(val osName: String) {
-    LINUX("linux-gnu"), DARWIN("darwin"), WINDOWS("windows"), CYGWIN("cygwin"), MSYS("msys"), FREEBSD("freebsd");
+    LINUX("linux"), DARWIN("darwin"), WINDOWS("windows"), CYGWIN("cygwin"), MSYS("msys"), FREEBSD("freebsd");
 
-    fun isUnixLike() = (this == LINUX || this == DARWIN || this == FREEBSD)
+    fun isUnixLike() = (this == LINUX || this == DARWIN || this == FREEBSD || this == CYGWIN || this == MSYS)
     fun isWindowsLike() = (this == WINDOWS)
     fun isUnixHostedOnWindows() = (this == CYGWIN || this == MSYS)
 
@@ -11,7 +11,8 @@ enum class OsType(val osName: String) {
         fun findOrThrow(name: String): OsType = find(name) ?: throw IllegalArgumentException("Unsupported OS: '$name'")
 
         // Exact comparison (it.osName.equals(name, true)) seems to be not feasible as there is also e.g. "darwin21"
-        // "darwin19" and maybe even other osTypes. It seems though that startsWith() is covering all cases.
+        // "darwin19", "linux-musl" (for Docker Alpine), "linux-gnu" and maybe even other osTypes. It seems though that
+        // startsWith() is covering all cases.
         // https://github.com/holgerbrandl/kscript/issues/356
         fun find(name: String): OsType? = values().find { name.startsWith(it.osName, true) }
     }
