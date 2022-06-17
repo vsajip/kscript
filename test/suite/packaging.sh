ce9eacc607e67577004e7212d4f1438374f97e1b
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2022-06-17 18:13:18 +0100

    Remove test executables after test.

--- test/suite/packaging.sh
+++ test/suite/packaging.sh
@@ -1,9 +1,9 @@
 ## can we resolve relative imports when using tmp-scripts  (see #95)
 assert "rm -f ${PROJECT_DIR}/test/package_example && kscript --package ${PROJECT_DIR}/test/resources/package_example.kts &>/dev/null && ${PROJECT_DIR}/test/package_example 1" "package_me_args_1_mem_536870912"
-
+rm -f "${PROJECT_DIR}/test/package_example"
 ## https://unix.stackexchange.com/questions/17064/how-to-print-only-last-column
-assert 'rm -f scriplet* && kscript --package "println(args.size)" >/dev/null 2>&1 && ./scriplet three arg uments' "3"
-
+assert 'rm -f scriplet* && kscript --package "println(args.size)" >/dev/null 2>&1 && ${PROJECT_DIR}/test/scriplet three arg uments' "3"
+rm -f "${PROJECT_DIR}/test/scriplet"
 #assert "kscript --package test/resources/package_example.kts" "foo"
 #assert "./package_example 1" "package_me_args_1_mem_4772593664"da
 #assert "echo 1" "package_me_args_1_mem_4772593664"
