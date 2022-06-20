94b6ddaf85b6cda53b0133015f0a2d5df79fff1a
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2022-06-20 17:54:11 +0100

    Pass OSTYPE in environment rather than command-line argument.

--- src/kscript.bat
+++ src/kscript.bat
@@ -4,5 +4,6 @@ for /f %%i in ('where kscript.bat') do set ABS_KSCRIPT_PATH=%%i
 set JAR_PATH=%ABS_KSCRIPT_PATH:~0,-4%.jar
 
 rem kotlin -classpath %JAR_PATH% kscript.app.KscriptKt "windows" %*
-FOR /F "tokens=* USEBACKQ" %%O IN (`kotlin -classpath %JAR_PATH% kscript.app.KscriptKt "windows" %*`) DO (SET RESULT=%%O)
+set OSTYPE=windows
+FOR /F "tokens=* USEBACKQ" %%O IN (`kotlin -classpath %JAR_PATH% kscript.app.KscriptKt  %*`) DO (SET RESULT=%%O)
 %RESULT%
